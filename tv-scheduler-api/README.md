dfb4e3184def45a675aef0c9344747e7db67f03b
Aston <aston.gilliland@outlook.com>
2015-04-10 08:52:46 +1000

    initial commit

---
+++ tv-scheduler-api/README.md
@@ -0,0 +1,3 @@
+﻿# wmc-scheduler
+
+
---
+++ tv-scheduler/js/app/directives/program-overview-directive.js
@@ -0,0 +1,48 @@
+﻿angular.module('tvSchedulerApp').directive('programOverview', function () {
+
+    function calculateMinutesInView(programStartTime, programEndTime, viewStartTime, viewEndTime) {
+
+        var startTime = moment.max(programStartTime, viewStartTime),
+            endTime = moment.min(programEndTime, viewEndTime);
+
+        return endTime.diff(startTime, 'minutes');
+
+    }
+
+    function link(scope, element) {
+
+        if (scope.viewStartTime && scope.viewEndTime) {
+
+            var totalViewMinutes = scope.viewEndTime.diff(scope.viewStartTime, 'minutes'),
+                minutesInView = calculateMinutesInView(scope.program.startTime,
+                                                       scope.program.endTime,
+                                                       scope.viewStartTime,
+                                                       scope.viewEndTime),
+                percentageInView = minutesInView / totalViewMinutes * 100;
+            element[0].style.width = percentageInView + '%';
+
+        }
+
+        var currentTime = moment();
+        if (scope.program.startTime.isBefore(currentTime)
+            && scope.program.endTime.isAfter(currentTime)) {
+
+            element.addClass('showing-now');
+
+        }
+
+    }
+
+    return {
+        restrict: 'E',
+        scope: {
+            program: '=model',
+            viewStartTime: '=viewStartTime',
+            viewEndTime: '=viewEndTime'
+        },
+        templateUrl: '/partials/program-overview.html',
+        replace: true,
+        link: link
+    };
+
+});
